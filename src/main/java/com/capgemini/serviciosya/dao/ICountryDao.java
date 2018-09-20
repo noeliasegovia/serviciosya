0c337ff51728712b4276ade2b9bf7cc6a8e5a381
noeliasegovia <noe0754@gmail.com>
2018-09-20 01:04:44 -0300

    el src va en api

--- src/main/java/com/capgemini/serviciosya/dao/ICountryDao.java
+++
@@ -1,6 +0,0 @@
-package com.capgemini.serviciosya.dao;
-
-import com.capgemini.serviciosya.beans.entity.CountryEntity;
-
-public interface ICountryDao extends IDao<CountryEntity, Integer> {
-}
--- src/main/java/com/capgemini/serviciosya/dao/IProvinceDao.java
+++
@@ -1,6 +0,0 @@
-package com.capgemini.serviciosya.dao;
-
-import com.capgemini.serviciosya.beans.entity.ProvinceEntity;
-
-public interface IProvinceDao extends IDao<ProvinceEntity, Integer>{
-}
